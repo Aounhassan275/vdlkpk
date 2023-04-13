d5439bfd16f412e684a3c54f4f062af4628711cf
Aounhassan275 <aounhassan275@gmail.com>
2023-04-13 14:06:02 +0500

    Fix

--- resources/views/ptgovkpk/home/index.blade.php
+++ resources/views/ptgovkpk/home/index.blade.php
@@ -12,10 +12,10 @@
     <div class="single-slider slider-height hero-overly slider-bg1 d-flex align-items-center" style="background-image:url({{asset($slider->image)}})!important;">
         <div class="container">
             <div class="row">
-                <div class="col-xxl-6 col-xl-7 col-lg-6 col-md-9">
+                <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12">
                     <div class="hero-caption">
-                        <h1 data-animation="pulse" data-delay=".4s">
-                        Verify Your<span>License</span>Now !</h1>
+                        <h1 data-animation="pulse" data-delay=".4s"  style="font-size:40px;">
+                        <span>Verify Your License Now !</span></h1>
                         <a href="{{url('verify_license')}}" class="hero-btn" data-animation="bounceIn" data-delay=".8s">Verify <i class="ti-angle-right"></i> </a>
                     </div>
                 </div>
@@ -28,10 +28,10 @@
     <div class="single-slider slider-height hero-overly slider-bg1 d-flex align-items-center">
         <div class="container">
             <div class="row">
-                <div class="col-xxl-6 col-xl-7 col-lg-6 col-md-9">
+                <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12">
                     <div class="hero-caption">
-                        <h1 data-animation="pulse" data-delay=".4s">
-                        Verify Your<span>License</span>Now !</h1>
+                        <h1 data-animation="pulse" data-delay=".4s"  style="font-size:40px;">
+                        <span>Verify Your License Now !</span></h1>
                         <a href="{{url('verify_license')}}" class="hero-btn" data-animation="bounceIn" data-delay=".8s">Verify <i class="ti-angle-right"></i> </a>
                     </div>
                 </div>
@@ -42,10 +42,10 @@
     <div class="single-slider slider-height hero-overly slider-bg2 d-flex align-items-center">
         <div class="container">
             <div class="row">
-                <div class="col-xxl-6 col-xl-7 col-lg-6 col-md-9">
+                <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12">
                     <div class="hero-caption">
-                        <h1 data-animation="pulse" data-delay=".4s">
-                        Verify Your<span>License</span>Now !</h1>
+                        <h1 data-animation="pulse" data-delay=".4s"  style="font-size:40px;">
+                        <span>Verify Your License Now !</span></h1>
                         <a href="{{url('verify_license')}}" class="hero-btn" data-animation="bounceIn" data-delay=".8s">Verify <i class="ti-angle-right"></i> </a>
                     </div>
                 </div>
@@ -56,10 +56,10 @@
     <div class="single-slider slider-height hero-overly slider-bg3 d-flex align-items-center">
         <div class="container">
             <div class="row">
-                <div class="col-xxl-6 col-xl-7 col-lg-6 col-md-9">
+                <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12">
                     <div class="hero-caption">
-                        <h1 data-animation="pulse" data-delay=".4s">
-                        Verify Your<span>License</span>Now !</h1>
+                        <h1 data-animation="pulse" data-delay=".4s"  style="font-size:40px;">
+                        <span>Verify Your License Now !</span></h1>
                         <a href="{{url('verify_license')}}" class="hero-btn" data-animation="bounceIn" data-delay=".8s">Verify <i class="ti-angle-right"></i> </a>
                     </div>
                 </div>
